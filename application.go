89acde596118179099f6cb77f8b1f0c08fa3d45f
iaroslavscript <yaroslav.akimov@gmail.com>
2021-11-10 19:29:36 +0200

    fix #9

--- application.go
+++ application.go
@@ -3,13 +3,16 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"os"
 	"time"
 )
 
 type Application struct {
-	ctx    context.Context
-	cancel context.CancelFunc
+	ctx        context.Context
+	cancel     context.CancelFunc
+	srv        http.Server
+	inShutdown bool
 }
 
 func NewApplication(ctx context.Context) *Application {
@@ -17,12 +20,19 @@ func NewApplication(ctx context.Context) *Application {
 	ctx, cancel := context.WithCancel(ctx)
 
 	return &Application{
-		ctx:    ctx,
-		cancel: cancel,
+		ctx:        ctx,
+		cancel:     cancel,
+		inShutdown: false,
 	}
 }
 
-func (a *Application) grathefullShoutdown(ctx context.Context) {
+func (a *Application) grathefullShutdown(ctx context.Context) error {
+	fmt.Println("enter grathefull shutdown state")
+	a.inShutdown = true
+	err := a.srv.Shutdown(ctx)
+
+	fmt.Println("exit grathefull shutdown state with error", err)
+	return err
 }
 
 func (a *Application) cleanup(s os.Signal) (bool, int) {
@@ -30,7 +40,7 @@ func (a *Application) cleanup(s os.Signal) (bool, int) {
 	ctx, cancel := context.WithTimeout(a.ctx, time.Duration(30000)*time.Millisecond)
 	defer cancel()
 
-	a.grathefullShoutdown(ctx)
+	a.grathefullShutdown(ctx)
 
 	return true, 0
 }
